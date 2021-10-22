c04d2938fe9264f0014a53c95d050a03e336893b
Daniel Vilajeti <daniel01.nyx@gmail.com>
2021-10-22 01:56:39 -0400

    hello world

---
+++ mcf_bot.py
@@ -0,0 +1,18 @@
+import os
+import discord
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+guild = discord.Guild
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == "mcf-bot":
+        response = "Hello! I am the MCF bot."
+        await message.channel.send
+    
+client.run(TOKEN)
